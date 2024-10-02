8a855e1ad795edecaff16cadef4469f0a3935ae4
funmi <sannifunmilola26@gmail.com>
2024-10-02 17:57:34 +0100

    commit

---
+++ ascending.py
@@ -0,0 +1,15 @@
+number_0f_times = int(input("The number of times: "))
+count = 1
+
+while(count >= number_0f_times):
+    number = int(input("The number: "))
+count +1
+
+if (number1 < number2 and number3):
+    lowest = number1
+
+if (number2 < number3 and number1):
+     secondLargest = number2
+
+else:
+    largest = number3
---
+++ test.py
@@ -0,0 +1,5 @@
+number1 = int(input("The first number : "))
+number2 = int(input("The second number : "))
+
+total = 0
+total = Math.pow(number1, number2)
